01b0155ad9c55351a3c05cf5a2c88b4fbad7209d
harini1sigc <202101035@sigc.edu>
2024-03-09 17:40:24 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://sheet2site.com/how-to-create-job-board/header.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
